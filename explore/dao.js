ed4c8611be5b5d0bfc5f371b69aeb6f97ea3d527
Dae Hyun <daehyun@Daes-MacBook-Pro.local>
2023-12-08 14:05:22 +0900

    explore db

--- explore/dao.js
+++ explore/dao.js
@@ -1,7 +1,7 @@
 import model from "./model.js";
 
 export const findAllObservationsDao = () => model.find();
-export const findObservationByIdDao = (id) => model.findById(id);
+export const findObservationByIdDao = (id) => model.findOne({ id: id });
 export const findObservationByCredentialDao = (username, password) => model.findOne({ username: username, password: password });  
 export const findObservationByRoleDao = (role) => model.find({ role: role }); 
 export const findObservationByUserName = (name) => model.findOne({ name: name });
--- explore/routes.js
+++ explore/routes.js
@@ -2,31 +2,6 @@ import Database from "../Database/index.js";
 import * as dao from "./dao.js";
 
 function exploreRoutes(app) {
-    // app.get("/project/explore", async (req, res) => {
-    //     const observations = Database.observations;
-    //     res.json(observations);
-    // });
-    // app.get("/project/explore/:id", async (req, res) => {
-    //     const id = parseInt(req.params.id, 10);
-    //     const observation = Database.observations.find((observation) => observation.id === id);
-    //     res.json(observation);
-    // });
-    // app.delete("/project/explore/:id", async (req, res) => {
-    //     const id = parseInt(req.params.id, 10);
-    //     const index = Database.observations.findIndex((observation)=> observation.id === id)
-    //     Database.observations.splice(index, 1);
-    //     res.json(204);
-    // });
-
-    // app.post("/project/explore", async (req, res) => {
-    //     const newObservation = {
-    //         id: new Date().getTime().toString(),
-    //         ...req.body,
-    //     };
-    //     Database.observations.unshift(newObservation);
-    //     res.json(newObservation);
-    // });
-
     const findAllObservations = async (req, res) => {
         const observations = await dao.findAllObservationsDao();
         res.json(observations);
@@ -35,6 +10,7 @@ function exploreRoutes(app) {
 
     const findObservationById = async (req, res) => {
         const id = parseInt(req.params.id, 10);
+        console.log(id);
         const observation = await dao.findObservationByIdDao(id);
         res.json(observation);
     }
